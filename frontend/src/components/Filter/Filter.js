362603827907e30fd870137b701a09f9ae20ff6f
shurikandrew <s26865@pjwstk.edu.pl>
2024-08-04 19:26:16 +0200

    Filter functionality using Slice Added

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,7 +1,26 @@
+import {useDispatch, useSelector} from "react-redux";
+import {selectTitleFilter, setTitleFilter} from "../../redux/slices/filterSlice";
+import './Filter.css';
+
 const Filter = ()=>{
+    const dispatch = useDispatch();
+    const handleTitleFilterChange = (e) => {
+        dispatch(setTitleFilter(e.target.value));
+    }
+
+    const titleFilter = useSelector(selectTitleFilter);
+
+
     return(
         <div className= "app-block filter">
-            <h2>Book Filter</h2>
+            <div className='filter-group'>
+                <input
+                    type="text"
+                    placeholder="Filter by title..."
+                    onChange={handleTitleFilterChange}
+                    value={titleFilter}
+                />
+            </div>
         </div>
     )
 }
